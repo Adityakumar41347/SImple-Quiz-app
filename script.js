2539c83f8b5d64d24dd68ff2ee7d828046339bb7
Aditya Thakur <adityakumar41347@gmail.com>
2025-08-30 17:19:52 +0530

    adding timer functionality

--- script.js
+++ script.js
@@ -69,17 +69,42 @@ quiz_data = [
 let currentQuestion = 0;
 
 function showQuestion() {
+
     const element = quiz_data[currentQuestion];
     document.getElementsByClassName("question")[0].innerHTML = element.question;
 
     element.options.forEach((el, i) => {
         document.getElementById(`option${i + 1}`).innerHTML = el;
     });
+    
+
 }
 let correct = 0
+let time = 600
+function timer() {
+    
+    setInterval(() => {
+        
+        if (time == 0) {
+            showresult();
+            
+            
+        }
+        if(time>0){
+         showtime()
+        }
+        
+        
+        time--;
+
+    }, 1000);
+}
+
+
 function submitanswer(obj) {
+
     if (currentQuestion >= quiz_data.length - 1) {
-        console.log("done")
+        
         showresult()
         return
     }
@@ -89,58 +114,67 @@ function submitanswer(obj) {
         showQuestion()
     }
     else {
-        console.log("wrong")
+        
         currentQuestion++;
         showQuestion()
     }
 
 
 }
- let saveinnerHTML
+function formatTime(seconds) {
+    const mins = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${String(mins).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
+}
+function showtime(){
+let vistime=formatTime(time)
+
+document.getElementsByClassName("timer")[0].innerHTML=vistime
+
+document.getElementsByClassName("timer")[0].style.color="red"
+}
+let saveinnerHTML
 function showresult() {
-    saveinnerHTML=document.getElementsByClassName("container")[0].innerHTML
+    saveinnerHTML = document.getElementsByClassName("container")[0].innerHTML
     const show = document.createElement("h1")
     show.innerHTML = `${correct}/${quiz_data.length}`
     show.style.width = "100%";
     show.style.height = "100%";
-    show.style.display="flex";
-    show.style.justifyContent="center";
-    show.style.alignItems="center";
-    show.style.fontSize="100px"
-    show.style.color="white"
-    const again=document.createElement("button")
-    again.innerHTML="try again"
-    again.style.backgroundColor="gray"
-    show.setAttribute("class","show")
+    show.style.display = "flex";
+    show.style.justifyContent = "center";
+    show.style.alignItems = "center";
+    show.style.fontSize = "100px"
+    show.style.color = "white"
+    const again = document.createElement("button")
+    again.innerHTML = "try again"
+    again.style.backgroundColor = "gray"
+    show.setAttribute("class", "show")
 
-    
+
     if (correct > quiz_data.length / 2) {
         show.style.backgroundColor = "green";
-        
+
     }
-    else{
-        show.style.backgroundColor="red"
+    else {
+        show.style.backgroundColor = "red"
     }
     const container = document.getElementsByClassName("container")[0];
     container.innerHTML = ""; // clear previous content
-    container.appendChild(show); 
+    container.appendChild(show);
     container.appendChild(again)
     again.setAttribute("onclick", "startagain()");
 
 
 }
-function startagain(){
-    currentQuestion=0
-    correct=0
-    console.log(saveinnerHTML)
+function startagain() {
+    currentQuestion = 0
+    correct = 0
+    time=600
     const container = document.getElementsByClassName("container")[0];
-    container.innerHTML = ""; 
-    
-    container.innerHTML=saveinnerHTML
-    console.log(container)
-    
+    container.innerHTML = "";
+    container.innerHTML = saveinnerHTML
     showQuestion()
-
 }
 
 showQuestion()
+timer()
